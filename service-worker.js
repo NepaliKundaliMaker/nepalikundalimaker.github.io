3e3011370876251e1d98cb5083dc8c6e896ff481
NepaliKundaliMaker <mickeyhaller.notguilty@gmail.com>
2025-07-13 13:19:05 +0545

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,20 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('kundali-cache').then(cache =>
+      cache.addAll([
+        'index.html',
+        'style.css',
+        'main.js',
+        'districts.js',
+        'manifest.json',
+        'icon-192.png'
+      ])
+    )
+  );
+});
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
